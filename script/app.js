613c4a8a49db4ea987135636f44e2b1be73ea6fc
Wufan Zhai <mail@zhaiwufan.com>
2016-09-09 15:39:19 +0800

    fix fontawesome request  longtime and add role

--- script/app.js
+++ script/app.js
@@ -3,5 +3,5 @@
  */
 
 if (window.location.protocol != "https:"){
-    window.location.href = "https://" + window.location.hostname;
+    //window.location.href = "https://" + window.location.hostname;
 }
